5d3f9e03a2f83274f16bfd48dab014ba22c96ddf
hoangnguyenthai <hoang.nguyenthai@gameloft.com>
2020-03-18 03:07:54 +0700

    +Done tutorial

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text counttext;
+    public Text wintext;
+    private Rigidbody rb;
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        wintext.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVeritcal = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVeritcal);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        counttext.text = "Count: " + count.ToString();
+        if(count >= 10)
+        {
+            wintext.text = "You win";
+        }
+    }
+}
