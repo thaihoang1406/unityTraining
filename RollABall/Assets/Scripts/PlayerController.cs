939a2b4f4486461e14fb94cdd92005dd3007a77d
hoangnguyenthai <hoang.nguyenthai@gameloft.com>
2020-03-18 17:07:57 +0700

    +Task: Move and take cube

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -2,12 +2,16 @@
 using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public GameObject PickUpFrefab;
+    public LayerMask layermask;
     public Text counttext;
     public Text wintext;
+
+    private int index;
+    private List<GameObject> listPos;
     private Rigidbody rb;
     private int count;
     // Start is called before the first frame update
@@ -15,24 +19,38 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        index = 0;
         SetCountText();
         wintext.text = "";
+        listPos = new List<GameObject>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVeritcal = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVeritcal);
-        rb.AddForce(movement * speed);
+        RaycastHit hit;
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit, 100, layermask);
+            GameObject temp = Instantiate(PickUpFrefab, hit.point, Quaternion.identity);
+            temp.SetActive(true);
+            listPos.Add(temp);
+        }
+        if (listPos.Count > index)
+            if (listPos[index].active == true)
+                transform.position = Vector3.MoveTowards(transform.position, listPos[index].transform.position, 0.1f);
+            else
+                index++;
+        
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PickUp"))
         {
-            other.gameObject.SetActive(false);
+            other.gameObject.transform.parent.gameObject.SetActive(false);
             count++;
             SetCountText();
         }
