141b0f46eaca434328ffbb4456f5154c4c6ec968
hoangnguyenthai <hoang.nguyenthai@gameloft.com>
2020-03-24 09:02:08 +0700

    +Add untiy particle pack and some effect for player

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,14 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public GameObject PickUpFrefab;
     public GameObject PickUpDieParticle;
+    public GameObject PlayerRespawnParticle;
+    public GameObject PlayerDieParticle;
     public LayerMask layermask;
     public Text counttext;
     public Text wintext;
 
-
+    private float currentTime;
+    private int state;
     private int index;
     private List<GameObject> listPos;
     private Rigidbody rb;
@@ -19,6 +22,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        state = 0;
         rb = GetComponent<Rigidbody>();
         count = 0;
         index = 0;
@@ -27,7 +31,7 @@ public class PlayerController : MonoBehaviour
         listPos = new List<GameObject>();
     }
 
-    void Update()
+    void initPickUp()
     {
         RaycastHit hit;
         if (Input.GetMouseButtonUp(0))
@@ -56,6 +60,37 @@ public class PlayerController : MonoBehaviour
             temp.SetActive(true);
             listPos.Add(temp);
         }
+    }
+
+    void Update()
+    {
+        initPickUp();
+
+        if(state == 1)
+        {
+            currentTime = Time.time;
+            state = 2;
+        }
+
+        if(state == 2)
+        {
+            if (Time.time - currentTime > 2)
+            {
+                gameObject.transform.position = new Vector3(0, 0, 0);
+                GameObject temp = Instantiate(PlayerRespawnParticle, gameObject.transform.position, Quaternion.identity);
+                temp.transform.position = new Vector3(temp.transform.position.x, temp.transform.position.y + 1, temp.transform.position.z);
+                currentTime = Time.time;
+                state = 3;
+            }
+        }
+
+        if (state == 3)
+        {
+            if (Time.time - currentTime > 2)
+            {
+                state = 0;
+            }
+        }
 
         checkAndPlayIdle();
     }
@@ -63,21 +98,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-       
-        
+        if (state == 0)
+        {
+            if (listPos.Count > index)
+                if (listPos[index].GetComponentInChildren<Rotator>().isdie == false)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, listPos[index].transform.position, 0.05f);
+                    transform.LookAt(listPos[index].transform);
+                    GetComponent<Animator>().SetTrigger("Walk");}
+                else
+                    index++;
+        }
 
-        if (listPos.Count > index)
-            if (listPos[index].GetComponentInChildren<Rotator>().isdie == false)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, listPos[index].transform.position, 0.05f);
-                transform.LookAt(listPos[index].transform);
-                GetComponent<Animator>().SetTrigger("Walk");
-                //transform.localScale = new Vector3(1, 1, 1);
-                //transform.parent.transform.position = Vector3.MoveTowards(transform.parent.transform.position, listPos[index].transform.position, 0.1f);
-            }
-            else
-                index++;
-        
     }
 
     void checkAndPlayIdle()
@@ -103,8 +135,18 @@ public class PlayerController : MonoBehaviour
             other.gameObject.GetComponent<Animator>().SetTrigger("Die");
 
             if(other.gameObject.GetComponent<Rotator>().isRed)
+            {
+                //die
+                state = 1;
+                GetComponent<Animator>().SetTrigger("Idle");
                 Instantiate(PickUpDieParticle,other.gameObject.transform.position, Quaternion.identity);
+                Instantiate(PlayerDieParticle, gameObject.transform.position, Quaternion.identity);
+                
+
+            }
+
             
+
             other.gameObject.GetComponent<Rotator>().isdie = true;
             count++;
             SetCountText();
